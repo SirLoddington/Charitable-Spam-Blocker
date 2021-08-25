32cac8f660d3d81a33ec3fbab93edd6d4fe3612a
Eric Daams <eric@164a.com>
2021-08-25 16:02:18 +0930

    Updated build

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -32,101 +32,11 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => 'd23729d419c655cd6c7c61f26c060e3f2cc809dc',
+    'reference' => '7cbe965c649b89ea5cd684b95846043097ac2e52',
     'name' => 'wpcharitable/charitable-spamblocker',
   ),
   'versions' => 
   array (
-    'gettext/gettext' => 
-    array (
-      'pretty_version' => 'v4.8.4',
-      'version' => '4.8.4.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '58bc0f7f37e78efb0f9758f93d4a0f669f0f84a1',
-    ),
-    'gettext/languages' => 
-    array (
-      'pretty_version' => '2.6.0',
-      'version' => '2.6.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '38ea0482f649e0802e475f0ed19fa993bcb7a618',
-    ),
-    'mck89/peast' => 
-    array (
-      'pretty_version' => 'v1.12.0',
-      'version' => '1.12.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '833be7a294627a8c5b1c482cbf489f73bf9b8086',
-    ),
-    'mustache/mustache' => 
-    array (
-      'pretty_version' => 'v2.13.0',
-      'version' => '2.13.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'e95c5a008c23d3151d59ea72484d4f72049ab7f4',
-    ),
-    'rmccue/requests' => 
-    array (
-      'pretty_version' => 'v1.8.0',
-      'version' => '1.8.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'afbe4790e4def03581c4a0963a1e8aa01f6030f1',
-    ),
-    'symfony/finder' => 
-    array (
-      'pretty_version' => 'v5.2.8',
-      'version' => '5.2.8.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'eccb8be70d7a6a2230d05f6ecede40f3fdd9e252',
-    ),
-    'wp-cli/i18n-command' => 
-    array (
-      'pretty_version' => 'v2.2.6',
-      'version' => '2.2.6.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'a66da3f09f6a728832381012848c3074bf1635c8',
-    ),
-    'wp-cli/mustangostang-spyc' => 
-    array (
-      'pretty_version' => '0.6.3',
-      'version' => '0.6.3.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '6aa0b4da69ce9e9a2c8402dab8d43cf32c581cc7',
-    ),
-    'wp-cli/php-cli-tools' => 
-    array (
-      'pretty_version' => 'v0.11.12',
-      'version' => '0.11.12.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'e472e08489f7504d9e8c5c5a057e1419cd1b2b3e',
-    ),
-    'wp-cli/wp-cli' => 
-    array (
-      'pretty_version' => 'v2.4.1',
-      'version' => '2.4.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'ceb18598e79befa9b2a37a51efbb34910628988b',
-    ),
     'wpcharitable/charitable-extension-activation' => 
     array (
       'pretty_version' => '1.1.2',
@@ -143,7 +53,7 @@ private static $installed = array (
       'aliases' => 
       array (
       ),
-      'reference' => 'd23729d419c655cd6c7c61f26c060e3f2cc809dc',
+      'reference' => '7cbe965c649b89ea5cd684b95846043097ac2e52',
     ),
   ),
 );
